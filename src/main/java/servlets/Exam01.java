ac406072e4f3d4c9d3ffc5b91d4245528ece5bfa
dOcOb <abg1418@gmail.com>
2022-10-09 01:09:54 +0900

    changepakage

--- src/main/java/servlets/Exam01.java
+++ src/main/java/servlets/Exam01.java
@@ -6,7 +6,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+@WebServlet //이 애노테이션 쓰는 법.
 public class Exam01 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
